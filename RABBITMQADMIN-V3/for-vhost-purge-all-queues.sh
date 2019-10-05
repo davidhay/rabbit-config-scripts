7d962e48592b469858098a043d42436dc3f9ceea
David Hay <david.j.hay@gmail.com>
2019-10-05 22:00:40 +0100

    purge all queues for vhost script

---
+++ RABBITMQADMIN-V3/for-vhost-purge-all-queues.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source ./common.sh
+
+getOptsUsernamePasswordVhost $@
+
+vhostQueues=$(rabbitadmForVhost -f bash list queues)
+if [[ "No items" = $vhostQueues ]]; then
+	echo "There are no queues for vhost [$VHOST]"
+	exit -1
+fi
+echo "BEFORE"
+rabbitadmForVhost list queues vhost name messages
+
+for vhostQueue in $vhostQueues; do
+	rabbitadmForVhost -q purge queue name="$vhostQueue"
+done
+
+for x in 5 4 3 2 1; do
+  echo -n "$x "
+  sleep 1
+done
+
+echo "AFTER"
+rabbitadmForVhost list queues vhost name messages
